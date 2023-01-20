17004b173394b64b12a742867dbf8c9ece9f85cb
mkhoirulwafa18 <muhammad.wafa@modalrakyat.id>
2023-01-20 10:09:23 +0700

    remove unused code

--- lib/themes/bloc/themes_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'themes_bloc.dart';
-
-class ThemesState extends Equatable {
-  const ThemesState({required this.themeData});
-
-  final ThemeData themeData;
-  @override
-  List<Object?> get props => [themeData];
-}

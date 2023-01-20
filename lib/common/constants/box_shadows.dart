dbb81fd7637223378e18e281a6c37b800d642acc
mkhoirulwafa18 <muhammad.wafa@modalrakyat.id>
2023-01-20 19:47:28 +0700

    add constants for praying time and box shadows

---
+++ lib/common/constants/box_shadows.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppShadow {
+  BoxShadow primaryShadow = BoxShadow(
+    color: Colors.black.withOpacity(0.5),
+    blurRadius: 4,
+    offset: const Offset(0, 4),
+  );
+}
--- lib/common/constants/constant.dart
+++ lib/common/constants/constant.dart
@@ -1,3 +1,18 @@
-const String randomSurah = 'noRandomSurah';
-const String randomAyat = 'noRandomAyat';
-const String lastHitAotd = 'lastHitAotd';
+class Constants {
+  final String randomSurah = 'noRandomSurah';
+  final String randomAyat = 'noRandomAyat';
+  final String lastHitAotd = 'lastHitAotd';
+  final shalats = [
+    'Subuh',
+    'Matahari Terbit',
+    'Dzuhur',
+    'Ashar',
+    'Matahari Terbenam',
+    'Maghrib',
+    'Isha',
+    'Imsak',
+    'Tengah Malam',
+    'Awal Sepertiga Malam',
+    'Akhir Sepertiga Malam',
+  ];
+}

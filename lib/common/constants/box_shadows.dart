6ff07c5df3e09c190e3be11f977b9d3db0924ab5
mkhoirulwafa18 <muhammad.wafa@modalrakyat.id>
2023-01-23 22:41:52 +0700

    remove app shadow, colors, and textstyles file

--- lib/common/constants/box_shadows.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppShadow {
-  BoxShadow primaryShadow = BoxShadow(
-    color: Colors.black.withOpacity(0.5),
-    blurRadius: 4,
-    offset: const Offset(0, 4),
-  );
-}

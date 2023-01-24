c520973b134dfa3b09f2fe3420194138af7f9d4c
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-01-24 23:41:50 +0700

    add prayer time cubit

--- lib/modules/prayer_time/cubit/prayertime_state.dart
+++ lib/modules/prayer_time/cubit/prayertime_state.dart
@@ -8,3 +8,20 @@ abstract class PrayertimeState extends Equatable {
 }
 
 class PrayertimeInitial extends PrayertimeState {}
+
+class PrayertimeLoading extends PrayertimeInitial {}
+
+class PrayertimeLoaded extends PrayertimeInitial {
+  PrayertimeLoaded({required this.data, required this.city});
+
+  final PrayerTime data;
+  final String city;
+  @override
+  List<Object> get props => [data, city];
+}
+
+class PrayertimeError extends PrayertimeInitial {
+  PrayertimeError({required this.error});
+
+  final String error;
+}

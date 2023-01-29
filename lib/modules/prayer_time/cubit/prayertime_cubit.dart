537d3f58e67c2edda74c37dcaa757957547ad6d3
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-01-29 10:10:14 +0700

    remove unnecessary logging

--- lib/modules/prayer_time/cubit/prayertime_cubit.dart
+++ lib/modules/prayer_time/cubit/prayertime_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:quran_app/modules/prayer_time/models/prayer_time.dart';
 import 'package:quran_app/services/dio.dart';
 
@@ -13,10 +12,7 @@ class PrayertimeCubit extends Cubit<PrayertimeState> {
   Future<void> getTimings(String month, String year) async {
     emit(PrayertimeLoading());
     final response = await dioHelper.getTimings(month, year);
-    debugPrint('udah dapet timings');
     final city = await dioHelper.getCityName();
-    debugPrint('udah dapet city: $city');
-    debugPrint('response code: ${response.code}');
     response.code == 200
         ? emit(PrayertimeLoaded(data: response, city: city))
         : emit(PrayertimeError(error: response.toString()));

4837e9fa8615c3af2b63555d40a3a6e51e0b6e9f
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-02-04 00:37:46 +0700

    added prev state by override onchange method in datepicker cubit

--- lib/modules/prayer_time/cubit/datepicker_cubit.dart
+++ lib/modules/prayer_time/cubit/datepicker_cubit.dart
@@ -3,7 +3,15 @@ import 'package:bloc/bloc.dart';
 class DatepickerCubit extends Cubit<DateTime> {
   DatepickerCubit() : super(DateTime.now());
 
+  DateTime? previousState = DateTime.now();
+
   void onSelectDate(DateTime selected) {
     emit(selected);
+  }
+
+  @override
+  void onChange(Change<DateTime> change) {
+    super.onChange(change);
+    previousState = change.currentState;
   }
 }

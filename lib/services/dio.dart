64a2363bc480226dede70fadcfd2fa7e1b3f0fcd
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-02-06 21:03:10 +0700

    add caching mechanism to response api

--- lib/services/dio.dart
+++ lib/services/dio.dart
@@ -1,18 +1,53 @@
 // ignore_for_file: inference_failure_on_instance_creation
 
+import 'dart:io';
+
+import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:quran_app/common/constants/constant.dart';
 
 import 'package:quran_app/modules/prayer_time/models/prayer_time.dart';
+import 'package:quran_app/services/app_path_provider.dart';
 
 class DioHelper {
   DioHelper() {
     _dio = Dio(
       BaseOptions(
-        baseUrl: 'http://api.aladhan.com/v1/calendarByCity',
+        baseUrl: Constants().baseUrl,
       ),
     );
+    _dio.interceptors.add(
+      DioCacheInterceptor(
+        options: CacheOptions(
+          store: HiveCacheStore(
+            AppPathProvider.path,
+            hiveBoxName: 'prayer_time_box',
+          ),
+          policy: CachePolicy.refreshForceCache,
+          maxStale: Duration(
+            days: Constants().cacheDays,
+          ),
+          priority: CachePriority.high,
+          hitCacheOnErrorExcept: [401, 404],
+          keyBuilder: (request) {
+            return request.uri.toString();
+          },
+        ),
+      ),
+    );
+    //this is for avoiding certificates error cause by dio
+    //https://issueexplorer.com/issue/flutterchina/dio/1285
+
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (HttpClient client) {
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+      return client;
+    };
     _dio.interceptors
         .add(LogInterceptor(requestBody: true, responseBody: true));
   }
@@ -27,9 +62,10 @@ class DioHelper {
       });
 
       final city = await getCityName();
+      final country = await getCountryName();
       // ignore: inference_failure_on_function_invocation
       final response = await _dio.get(
-        '?city=$city&country=Indonesia&method=11&month=$month&year=$year',
+        '?city=$city&country=$country&method=11&month=$month&year=$year',
       );
       final result = PrayerTime.fromJson(response.data as Map<String, dynamic>);
       return result;
@@ -45,6 +81,17 @@ class DioHelper {
         long,
       );
       return placemarks[0].subAdministrativeArea.toString();
+    }
+    return 'Lokasi tidak ditemukan';
+  }
+
+  Future<String> getCountryName() async {
+    if (lat != 0 && long != 0) {
+      final placemarks = await placemarkFromCoordinates(
+        lat,
+        long,
+      );
+      return placemarks[0].country.toString();
     }
     return 'Lokasi tidak ditemukan';
   }
@@ -67,7 +114,10 @@ class DioHelper {
     if (permission == LocationPermission.deniedForever) {
       await Geolocator.openAppSettings();
       await Future.error(
-        'Lokasi tidak diizinkan selamanya, mohon izinkan akses lokasi untuk mengakses fitur ini.',
+        '''
+Lokasi tidak diizinkan selamanya,
+Mohon izinkan akses lokasi untuk mengakses fitur ini.
+        ''',
       );
     }
 

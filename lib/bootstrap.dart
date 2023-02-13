9eef1f16a98003bf06f254e3bf516d44c634b178
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-02-13 17:10:39 +0700

    set statusbar in bootstrap.dart

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,8 +2,9 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:quran_app/common/constants/constant.dart';
 import 'package:quran_app/services/app_path_provider.dart';
 
 class AppBlocObserver extends BlocObserver {
@@ -28,6 +29,7 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   Bloc.observer = AppBlocObserver();
   await ScreenUtil.ensureScreenSize();
   await AppPathProvider.initPath();
+  setStatusBar(color: backgroundColor2, brightness: Brightness.light);
   await runZonedGuarded(
     () async => runApp(await builder()),
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),

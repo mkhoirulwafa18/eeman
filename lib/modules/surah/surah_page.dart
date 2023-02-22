3b48e115f8be7f3f0782e48cea9e353e7ff968f7
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-02-22 12:30:19 +0700

    implement multiblocprovider and use surah info cubit in surah page

--- lib/modules/surah/surah_page.dart
+++ lib/modules/surah/surah_page.dart
@@ -5,14 +5,15 @@ import 'package:quran_app/common/widgets/app_loading.dart';
 import 'package:quran_app/common/widgets/base_page.dart';
 import 'package:quran_app/common/widgets/custom_app_bar.dart';
 import 'package:quran_app/modules/surah/cubit/murattal_cubit.dart';
+import 'package:quran_app/modules/surah/cubit/surah_info_cubit.dart';
 import 'package:quran_app/modules/surah/widgets/action_button.dart';
 import 'package:quran_app/modules/surah/widgets/surah_info.dart';
 import 'package:quran_app/modules/surah_list/models/quran.dart';
 import 'package:quran_app/modules/surah_list/widgets/rub_el_hizb.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
-class SurahPage extends StatefulWidget {
-  const SurahPage({
+class SurahPage extends StatelessWidget {
+  SurahPage({
     super.key,
     required this.noSurah,
     required this.dataQuran,
@@ -21,94 +22,77 @@ class SurahPage extends StatefulWidget {
   final int noSurah;
   final List<Quran> dataQuran;
   final int? startAyat;
-  @override
-  State<SurahPage> createState() => _SurahPageState();
-}
-
-class _SurahPageState extends State<SurahPage> {
-  List<Verse> _ayatSurah = [];
-  int _indexSurah = 0;
-  String title = '';
-  int numberSurah = 0;
-  String translation = '';
-  Type revelation = Type.MAKKIYAH;
-  int totalAyat = 0;
-  int lastReadAyat = 0;
-
-  Future<void> initialState() async {
-    final data = widget.dataQuran;
-    setState(() {
-      _indexSurah = widget.noSurah;
-      _ayatSurah = data[_indexSurah].verses ?? [];
-      title = data[_indexSurah].name ?? '';
-      numberSurah = data[_indexSurah].numberOfSurah ?? 0;
-      translation = data[_indexSurah].nameTranslations!.id ?? '';
-      revelation = data[_indexSurah].type ?? Type.MAKKIYAH;
-      totalAyat = data[_indexSurah].numberOfAyah ?? 0;
-    });
-  }
-
   final controller = ItemScrollController();
-  @override
-  void initState() {
-    super.initState();
-    initialState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => MurattalCubit(),
-      child: BasePage.noPadding(
-        appBar: CustomAppBar(title: title),
-        child: _ayatSurah.isNotEmpty
-            ? Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: ScrollablePositionedList.builder(
-                  itemScrollController: controller,
-                  shrinkWrap: true,
-                  itemPositionsListener: ItemPositionsListener.create(),
-                  itemCount: _ayatSurah.length + 2,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return SurahInfo(
-                        numberSurah: numberSurah,
-                        title: title,
-                        translation: translation,
-                        revelation: revelation.name,
-                        totalAyat: totalAyat,
-                      );
-                    }
-                    index -= 1;
-                    return _ayatSurah.length != index
-                        ? _buildItem(index)
-                        : _buildFooter();
-                  },
-                ),
-              )
-            : const AppLoading(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => MurattalCubit(),
+        ),
+        BlocProvider(
+          create: (context) => SurahInfoCubit()..init(noSurah, dataQuran),
+        ),
+      ],
+      child: BlocBuilder<SurahInfoCubit, SurahInfoState>(
+        builder: (context, state) {
+          if (state is SurahInfoLoaded) {
+            return BasePage.noPadding(
+              appBar: CustomAppBar(title: state.title),
+              child: state.ayatSurah.isNotEmpty
+                  ? Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: ScrollablePositionedList.builder(
+                        itemScrollController: controller,
+                        shrinkWrap: true,
+                        itemPositionsListener: ItemPositionsListener.create(),
+                        itemCount: state.ayatSurah.length + 2,
+                        itemBuilder: (context, index) {
+                          if (index == 0) {
+                            return SurahInfo(
+                              numberSurah: state.numberSurah,
+                              title: state.title,
+                              translation: state.translation,
+                              revelation: state.revelation.name,
+                              totalAyat: state.totalAyat,
+                            );
+                          }
+                          index -= 1;
+                          return state.ayatSurah.length != index
+                              ? _buildItem(index, state)
+                              : _buildFooter(state);
+                        },
+                      ),
+                    )
+                  : const AppLoading(),
+            );
+          } else {
+            return const AppLoading();
+          }
+        },
       ),
     );
   }
 
-  Padding _buildFooter() {
+  Padding _buildFooter(SurahInfoLoaded state) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          if (numberSurah != 1)
+          if (state.numberSurah != 1)
             ActionButton(
               type: ButtonActionType.back,
-              indexSurah: _indexSurah,
-              widget: widget,
+              indexSurah: noSurah,
+              dataQuran: dataQuran,
             )
           else
             const SizedBox(),
-          if (numberSurah != 114)
+          if (state.numberSurah != 114)
             ActionButton(
-              indexSurah: _indexSurah,
-              widget: widget,
+              indexSurah: noSurah,
+              dataQuran: dataQuran,
               type: ButtonActionType.next,
             )
           else
@@ -118,7 +102,7 @@ class _SurahPageState extends State<SurahPage> {
     );
   }
 
-  ListTile _buildItem(int index) {
+  ListTile _buildItem(int index, SurahInfoLoaded state) {
     return ListTile(
       dense: true,
       contentPadding: const EdgeInsets.all(16),
@@ -129,9 +113,9 @@ class _SurahPageState extends State<SurahPage> {
         text: TextSpan(
           children: [
             TextSpan(
-              text: (index == 0 && numberSurah != 1
-                      ? _ayatSurah[index].text!.substring(39)
-                      : _ayatSurah[index].text) ??
+              text: (index == 0 && state.numberSurah != 1
+                      ? state.ayatSurah[index].text!.substring(39)
+                      : state.ayatSurah[index].text) ??
                   '',
               style: arabicText,
             ),
@@ -153,11 +137,26 @@ class _SurahPageState extends State<SurahPage> {
           const SizedBox(
             height: 16,
           ),
-          Text(
-            _ayatSurah[index].translationId ?? '',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              color: backgroundColor2,
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: state.ayatSurah[index].translationId ?? '',
+                  style: smallText.copyWith(
+                    color: backgroundColor2,
+                  ),
+                ),
+                WidgetSpan(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(
+                      Icons.info,
+                      size: 15,
+                      color: backgroundColor2.withOpacity(.5),
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ],

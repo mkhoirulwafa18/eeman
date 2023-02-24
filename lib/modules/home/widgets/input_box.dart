4fc7b3fa533b3b8a1c8df6096ceafa5bab41a4a9
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-02-24 17:42:18 +0700

    add some required arguments in input box widget for searching ayah

--- lib/modules/home/widgets/input_box.dart
+++ lib/modules/home/widgets/input_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quran_app/common/constants/constant.dart';
 
 class InputBox extends StatelessWidget {
@@ -7,10 +8,20 @@ class InputBox extends StatelessWidget {
     this.onChanged,
     this.labelText,
     this.enabled = true,
+    this.autofocus = false,
+    this.formatter,
+    this.keyboardType,
+    this.maxLength,
+    this.controller,
   });
   final ValueChanged<String>? onChanged;
   final String? labelText;
   final bool enabled;
+  final bool autofocus;
+  final List<TextInputFormatter>? formatter;
+  final TextInputType? keyboardType;
+  final int? maxLength;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +34,13 @@ class InputBox extends StatelessWidget {
       child: TextFormField(
         enabled: enabled,
         cursorColor: backgroundColor,
+        controller: controller,
         onChanged: onChanged,
         style: inputText,
+        autofocus: autofocus,
+        inputFormatters: formatter,
+        keyboardType: keyboardType,
+        maxLength: maxLength,
         decoration: InputDecoration(
           isDense: true,
           labelText: labelText,
